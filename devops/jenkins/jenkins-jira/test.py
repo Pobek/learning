c315c4dd89bfafb70ae584674f49e101c89af95e
Sputters <coevyatar@gmail.com>
2019-05-02 19:37:05 +0300

    Jenkins: Add tests functionallity & comments

---
+++ devops/jenkins/jenkins-jira/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_nothing(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
