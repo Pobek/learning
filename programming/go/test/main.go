ae5a7c7c289d4cd87fc1673645545a6a6b80c9a1
Evyatar Cohen <evyatar.cohen@thomsonreuters.com>
2019-07-04 22:58:11 +0300

    Add Programming Learning

---
+++ programming/go/test/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a = 10;
+    if(a == 4) {
+        fmt.Println("Marco!")
+    }else{
+        a = 5;
+        fmt.Println(a)
+    }
+}
---
+++ programming/go/test/test.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    /*Comment*/
+    //fmt.Println("Hello")
+    var a int = 5;
+    var f bool = false;
+    b := 23.2;
+    const w, h = 10,5;
+    fmt.Println(a<5)
+    fmt.Println(f==false)
+    fmt.Println(b)
+    fmt.Println(w*h)
+}
